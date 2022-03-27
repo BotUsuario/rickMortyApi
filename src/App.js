4e65d94dcd026ef27a4e36dadbfb5303682fa9d1
Juan Eulufi <juan.eulufi.sa@gmail.com>
2022-03-27 12:42:16 -0300

    aplicando ternario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import imagenRichMorty from "./img/rick-morty.png"
-import './App.css';
+import imagenRichMorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
 
   const reqApi = async () => {
-    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const api = await fetch("https://rickandmortyapi.com/api/character");
     const characterApi = await api.json();
 
-    console.log(characterApi.results)
-  }
+    setCharacters(characterApi.results);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imagenRichMorty} className="img-home" alt="Rich & Morty" />
-        <button onClick={reqApi} className="btn-search">
-          Buscar Personajes
-        </button>
+        {characters ? (
+          <Characters characters={characters} />
+        ) : (
+          <>
+            <img
+              src={imagenRichMorty} className="img-home"alt="Rich & Morty"/>
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
---
+++ src/components/Characters.js
@@ -0,0 +1,8 @@
+export default function characters(props) {
+    console.log(props)
+  return (
+    <div>
+        <h1>Desde character JS</h1>
+    </div>
+  )
+}
